94b03b17ca4d3e58fcb1492c4c884c155ff71819
Md khurshid Jahan <khurshid.jahan@northsouth.edu>
2021-10-17 23:49:53 -0700

    Atcoder

---
+++ exact price.cpp
@@ -0,0 +1,16 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int x;
+    cin>>x;
+    if(x==0)
+        cout<<"No\n";
+    else if(x%100==0)
+        cout<<"Yes\n";
+    else
+        cout<<"No\n";
+    return 0;
+}
